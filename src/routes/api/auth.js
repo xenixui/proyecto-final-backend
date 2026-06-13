b66caba95f13c28ffaf2376f4b75af28160b78c2
Carlos García Abella <carlosg.abella@gmail.com>
2026-06-13 17:25:00 +0200

    borrar duplicados

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -4,7 +4,6 @@ const authMiddleware = require('../../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authMiddleware, authController.logout);
 router.get('/me', authMiddleware, authController.me);
