32ecc0627683171292f4c56007c03792eff669c6
Carlos García Abella <carlosg.abella@gmail.com>
2026-06-14 11:46:51 +0200

    yup controla las validaciones

--- src/controllers/articles.controller.js
+++ src/controllers/articles.controller.js
@@ -2,7 +2,9 @@ const articlesService = require('../services/articles.service');
 
 async function searchArticles(req, res) {
     try {
-        const result = await articlesService.searchArticles(req.query);
+        const result = await articlesService.searchArticles(
+            req.validatedQuery,
+        );
         return res.status(200).json(result);
     } catch (error) {
         return res.status(error.status).json(error.message);
