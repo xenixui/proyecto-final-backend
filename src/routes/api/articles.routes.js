2a3b057f99373d2d27ef7164e4e6e198a958bba9
xenixux <kseniakonnova.ux@gmail.com>
2026-06-15 19:28:16 +0200

    comentarios en rutas de articulos

--- src/routes/api/articles.routes.js
+++ src/routes/api/articles.routes.js
@@ -3,10 +3,14 @@ const router = require('express').Router();
 
 //Recuperar todos los artículos
 router.get('/', getAll); 
+
+//Filtar artículos
 router.get('/filter', filter);
+
+//Buscar artículos
 router.get('/search/:term', search);
+
+//Obtener artículo por ID
 router.get('/:article_id', getById);
-
-
 
 module.exports = router; 
