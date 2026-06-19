759541b5b4f0db9fd8f12efebe6573f1f73d76b4
Carlos Abella <carlosg.abella@gmail.com>
2026-06-19 18:50:27 +0200

    Revert "User Profiel login"

--- src/app.js
+++ src/app.js
@@ -14,11 +14,8 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/api/chats', chatRoutes);
 
-app.use('/api/auth', authRoutes);
-app.use("/api/profile", profileRoutes);
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Ruta no encontrada' });
+    res.status(404).json({ message: 'Ruta no encontrada' });
+app.use((_req, res) => {
 });
 
 app.use((error, _req, res) => {
