981555a9ece368c965c7c07c8aa22eb16f12096c
Miriam Nieto <miriam.nieto726@gmail.com>
2026-06-23 13:43:07 +0200

    Feature/admin gestion perfiles (#37)
    * muevo articles.rest a la carpeta raiz http
    
    * instalo yup
    
    * comentarios en rutas de articulos
    
    * creo el listado de las rutas de admin para perfiles
    
    * wip rutas admin
    
    * Unificación de archivos. Ver listado modificado y dar de alta hecho
    
    * Dar de baja y bloquear usuarios listo
    
    * Asignar y quitar rol hechos
    
    * ajustes
    
    * añadir schema para asignar roles
    
    * Creación de nueva función
    
    * arreglo
    
    ---------
    
    Co-authored-by: xenixux <kseniakonnova.ux@gmail.com>
    Co-authored-by: Miriam Nieto <miriam.nito726@gmail.com>

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,8 +1,14 @@
-const { query } = require('../config/database');
+const {
+    query
+} = require('../config/database');
+const profileModel = require('../models/profiles.model');
+const profileService = require('../services/profile.service')
 
 async function getProfileByUser(req, res) {
     try {
-        const { userId } = req.params;
+        const {
+            userId
+        } = req.params;
 
         const users = await query('SELECT id FROM users WHERE id = ? LIMIT 1', [
             userId,
@@ -28,6 +34,171 @@ async function getProfileByUser(req, res) {
         return res.status(error.status).json(error.message);
     }
 }
+
+async function getProfiles(req, res) {
+    try {
+
+        const {
+            rol
+        } = req.query
+
+        const result = rol ?
+            await profileModel.getProfilesByRole(rol.toLowerCase()) :
+            await profileModel.getAllProfiles();
+
+        return res.json(result);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error al recuperar los perfiles',
+            error: error.message
+        });
+    }
+
+}
+
+async function getProfileDetailById(req, res) {
+    try {
+        const result = await profileService.getProfileDetail(req.params.id)
+        if (!result) {
+            return res.status(404).json({
+                message: 'No existe un usuario con ese ID'
+            })
+        }
+        return res.json(result)
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error al recuperar el usuario'
+        })
+    }
+}
+
+// Dar de alta
+async function createUser(req, res) {
+    try {
+        const result = await profileService.createUserAsAdmin((req.body))
+
+        if (!result) {
+            return res.status(404).json({
+                message: 'Error al procesar la solicitud del usuario'
+            })
+        }
+        return res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error al crear o recuperar el usuario'
+        })
+    }
+}
+
+// dar de baja
+async function deleteUser(req, res) {
+    try {
+        const {
+            id
+        } = req.params
+
+        const result = await profileModel.deactivateUser(id)
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: 'No se encontró ningún usuario con ese ID para dar de baja'
+            });
+        }
+
+        res.json({
+            message: 'Usuario dado de baja correctamente'
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error al dar de baja al usuario'
+        });
+    }
+}
+
+// bloquear
+
+async function blockedUser(req, res) {
+    try {
+        const {
+            id
+        } = req.params
+        const result = await profileModel.blockUser(id)
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: 'No se encontró ningún usuario con ese ID para bloquear'
+            })
+        }
+        res.json({
+            message: 'Usuario bloqueado correctamente'
+        })
+
+    } catch (error) {
+
+        return res.status(500).json({
+            message: 'Error al bloquear al usuario'
+        });
+    }
+}
+
+async function assignedRole(req, res) {
+    try {
+
+        const {
+            id
+        } = req.params
+        const {
+            rol
+        } = req.body
+        const result = await profileModel.assignRole(id, rol)
+
+        if (!result) {
+            return res.status(404).json({
+                message: 'Rol no encontrado'
+            })
+        }
+
+        res.status(201).json({
+            message: 'Rol asignado correctamente'
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error al asignar un rol'
+        });
+    }
+}
+
+async function removedRole(req, res) {
+    try {
+        const {
+            id,
+            roleId
+        } = req.params
+
+        const result = await profileModel.removeRole(id, roleId)
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: 'No se encontró esa relación de rol para ese usuario'
+            })
+        }
+
+        res.status(200).json({
+            message: 'Rol eliminado correctamente'
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error al eliminar un rol'
+        });
+    }
+}
+
 
 // PUT /api/profile  →  actualiza los datos editables del perfil
 async function updateProfile(req, res, next) {
@@ -85,6 +256,13 @@ async function getMyChats(req, res, next) {
 
 module.exports = {
     getProfileByUser,
+    getProfiles,
+    getProfileDetailById,
+    createUser,
+    deleteUser,
+    blockedUser,
+    assignedRole,
+    removedRole,
     updateProfile,
     getMyArticles,
     getMyPurchases,
--- src/routes/api/articles.routes.js
+++ src/routes/api/articles.routes.js
@@ -6,8 +6,14 @@ const router = require('express').Router();
 
 //Recuperar todos los artículos
 router.get('/', getAll); 
+
+//Filtar artículos
 router.get('/filter', filter);
+
+//Buscar artículos
 router.get('/search/:term', search);
+
+//Obtener artículo por ID
 router.get('/:article_id', getById);
 router.post('/', authMiddleware, validateSchema(createArticleSchema), create);
 router.delete('/:article_id', authMiddleware, remove);
