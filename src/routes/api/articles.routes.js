87844f55da06fe644c1cb09db03fbe3b21884af9
NikyMC <nikymikeciolca@gmail.com>
2026-06-24 18:05:19 +0200

    Implementacion funcionalidad para marcar un producto como vendido (#42)
    * Implementacion funcionalidad para marcar un producto como vendido
    
    * Arreglo en el controller y organización en articles.routes

--- src/routes/api/articles.routes.js
+++ src/routes/api/articles.routes.js
@@ -6,7 +6,8 @@ const {
     filter,
     create,
     remove,
-    update
+    update,
+    markAsSold
 } = require('../../controllers/articles.controller');
 const authMiddleware = require('../../middlewares/auth.middleware');
 const { validateSchema } = require('../../middlewares/validation.middleware');
@@ -23,8 +24,17 @@ router.get('/user/:userId', authMiddleware, getByUserIdAndStatus);
 
 //Obtener artículo por ID
 router.get('/:article_id', getById);
+
+//Crear artículo
 router.post('/', authMiddleware, validateSchema(createArticleSchema), create);
+
+//Eliminar artículo
 router.delete('/:article_id', authMiddleware, remove);
+
+// Actualizar artículo
 router.put('/:article_id', authMiddleware, validateSchema(updateArticleSchema), update);
+
+//Marcar artículo como vendido
+router.patch('/:article_id/sold', authMiddleware, markAsSold);
 
 module.exports = router;
